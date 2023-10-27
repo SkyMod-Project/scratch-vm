8059962ecfeb7221c37248c27cdc7dc904f32ee1
CrossScar <crossscar2@outlook.com>
2023-10-27 07:45:11 -0700

    switch to full extension page

--- test/unit/tw_extension_manager.js
+++ test/unit/tw_extension_manager.js
@@ -15,9 +15,9 @@ test('_isValidExtensionURL', t => {
     const manager = new ExtensionManager(fakeRuntime);
     t.equal(manager._isValidExtensionURL('fetch'), false);
     t.equal(manager._isValidExtensionURL(''), false);
-    t.equal(manager._isValidExtensionURL('extensions.turbowarp.org/fetch.js'), false);
-    t.equal(manager._isValidExtensionURL('https://extensions.turbowarp.org/fetch.js'), true);
-    t.equal(manager._isValidExtensionURL('http://extensions.turbowarp.org/fetch.js'), true);
+    t.equal(manager._isValidExtensionURL('skymod-project.github.io/extensions/fetch.js'), false);
+    t.equal(manager._isValidExtensionURL('https://skymod-project.github.io/extensions/fetch.js'), true);
+    t.equal(manager._isValidExtensionURL('http://skymod-project.github.io/extensions/fetch.js'), true);
     t.equal(manager._isValidExtensionURL('http://localhost:8000'), true);
     t.equal(manager._isValidExtensionURL('data:application/javascript;base64,YWxlcnQoMSk='), true);
     t.equal(manager._isValidExtensionURL('file:///home/test/extension.js'), true);
